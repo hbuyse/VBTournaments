c9ff153fd4b61e78b6f9a4c4cad5e8694f19d468
Henri Buyse <henri.buyse@gmail.com>
2015-07-12 23:15:04 +0200

    Delete the import of "forms" in urls.py

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from . import views, forms
+from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
