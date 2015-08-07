cf510d3b4b0e6b38a2668f536a72e8d8a8f789d4
Henri Buyse <henri.buyse@gmail.com>
2015-08-07 10:05:31 +0200

    Update settings.py

--- vbtournaments/settings.py
+++ vbtournaments/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'easy_maps',
+    # 'easy_maps',
     'bootstrap3',
     'bootstrapform',
     'accounts',
